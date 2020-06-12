633d868bcddf78c26ef02165b4da122746c07ca7
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-06-12 18:25:45 +0000

    add new conditionals using logical operators

--- conditionals.js
+++ conditionals.js
@@ -23,14 +23,39 @@
 
 //++++++++++++++++++++++++++++++++++
 
-const temperature = 0;
+// const temperature = 0;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+//this is my test line
+//+++++++++++++++++++++
+
+// const isCitizen = false;
+// const age = 29;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.")
+// } else {
+//   console.log("Sorry, you can't vote this time.")
+// }
+
+//+++++++++++++++
+
+const temperature = 41;
+const raining = false;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
 
-console.log("Now you're ready to go outside!");
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
