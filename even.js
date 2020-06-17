2ad571e8a487cb41fbfd5e1e8c250db8adb8d7cb
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-06-17 19:34:09 +0000

    create even.js to follow instructions in Compass

---
+++ even.js
@@ -0,0 +1,11 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);  const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+console.log(isEven(7));
+console.log(isEven(8));
