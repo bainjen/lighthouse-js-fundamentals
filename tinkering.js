927c63831cfbec1ec3e5945fe2de71fc5748052f
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-06-21 23:01:01 +0000

    update chorus-for and tinkering js files

--- tinkering.js
+++ tinkering.js
@@ -310,32 +310,3 @@
 //      return laughSound;
 // });
     
-
-//function will receive an array of stations
-//each station will also be an array  with a name, capcity, venue type 
-//appropriate station -> capacity of >= 20, a school OR comminity center
-
-const stations = [
-    ['Big Bear Donair', 10, 'restaurant'],
-    ['Bright Lights Elementary', 50, 'school'],
-    ['Moose Mountain Community Centre', 45, 'community centre']
-  ];
-  
-
-const chooseStations = (stations) => {
-    // let name = stations[0];
-    // let capacity = stations[1];
-    // let locationType = stations [2];
-    let goodStations = [];
-
-    for (let i = 0; i < stations.length; i++) {
-
-        if (stations[1] >= 20 && stations[2] === "school" || stations[2] === "community centre") {
-           stations[0].push(goodStations);
-        }
-    }
-    return goodStations;
-}
-
-console.log(chooseStations(stations))
-
