40cb85306ecee9190a9f84d93527cd050ba1df4a
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-06-21 23:00:20 +0000

    create ice-cream and packing-list files to complete Compass bootcamp prep work on arrays. git status

---
+++ ice-cream.js
@@ -0,0 +1,9 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
